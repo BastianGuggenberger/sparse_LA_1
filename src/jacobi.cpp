3741cee8274ebd3243968b0bf75f5ab381ba5eb8
BastianGuggenberger <bastian.guggenberger@aon.at>
2025-12-07 18:29:38 +0100

    implement basic jacobi algorithm for an example matrix

--- src/jacobi.cpp
+++ src/jacobi.cpp
@@ -2,6 +2,10 @@
 #include <eigen3/Eigen/Dense>
 
 int main() {
+
+	int max_iterations = 500;
+	double accuracy = 1e-9;
+
 	Eigen::MatrixXd A(4, 4);
 	Eigen::VectorXd b(4);
 	Eigen::VectorXd x(4); //Solution of Ax = b
@@ -12,9 +16,33 @@ int main() {
 
 	b << 5,6,6,5;
 
-	for (int i=0; i<500; i++){
+	Eigen::MatrixXd M = A.diagonal().asDiagonal();
+	Eigen::MatrixXd M_inverse = M.inverse();
+
+	Eigen::VectorXd x_old(4); //For comparison
+	int n=0;
+
+	for(int i=0; i<max_iterations; i++){
+
+		//print current x
+		std:: cout << x[0];
+		for (int j=1; j<x.size(); j++){
+			std::cout << "," << x[j];
+		}
+		std::cout << std::endl;
+
+		//calculate new x
+		x_old = x;
+		x = x + M_inverse * (b - A * x);
+		n+=1;
+
+		//check for convergence
+		if((x-x_old).norm()<accuracy) break;
 		
+
 	}
 
+	std::cout << std::endl << n << " iterations" << std::endl;
+	
 	return 0;
 }
